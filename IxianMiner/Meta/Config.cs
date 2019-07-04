86aea7da99cd4b68384301be4c01cbb132704c27
MarkoAT <no-reply@ixian.io>
2019-07-04 22:06:21 +0100

    removed unnecessary includes

--- IxianMiner/Meta/Config.cs
+++ IxianMiner/Meta/Config.cs
@@ -1,10 +1,7 @@
 ﻿using Fclp;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IxianMiner
 {
