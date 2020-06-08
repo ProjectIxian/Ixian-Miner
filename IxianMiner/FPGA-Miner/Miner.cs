6469cc13711661bd7065f1b41bd029940330abb0
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-06-08 15:11:13 +0200

    Created Miner.cs at FPGA Miner
    I created the file Miner.cs in the new folder FPGA-Miner for the peoples who just want to mine Ixian with their FPGA Miners ( FPGA CPUs and FPGA GPUs )

---
+++ IxianMiner/FPGA-Miner/Miner.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace IxianMiner
+{
+    class Miner
+    {
+    
+    
+    
+    }
+    
+}
